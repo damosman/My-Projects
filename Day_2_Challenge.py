c42428170b2b78f9a6983e0c2c32d8c4f174ae58
Damilare Akinboboye <48088578+damosman@users.noreply.github.com>
2021-12-02 04:06:51 +0100

    Update Day_2_Challenge.py

--- Day_2_Challenge.py
+++ Day_2_Challenge.py
@@ -44,5 +44,54 @@ string15 = ("800")
 string16 = ("900")
 string17 = ("Well, let us try to think about that in another way")
 
+# print(len(string1))
+
+# the 5 repeated phrase
+
+slice1 = slice(0, 21)
+slice2 = slice(0, 21)
+slice3 = slice(0, 21)
+slice4 = slice(0, 21)
+slice5 = slice(0, 21,)
+
+# print statement for 5 repeated phrase
+print(string2[slice1])
+print(string5[slice2])
+print(string8[slice3])
+print(string11[slice4])
+print(string13[slice5])
+
+# the non-repeated phrase and print to the terminal
+slice6 = slice(0, 57)
+slice7 = slice(0, 27)
+slice8 = slice(0, 33)
+slice9 = slice(0, 37)
+slice10 = slice(0, 51)
+
+# print statement for the non-repeated phrase and print to the terminal
+print(string4[slice6])
+print(string7[slice7])
+print(string9[slice8])
+print(string12[slice9])
+print(string17[slice10])
+
+# all the numbers
+slice11 = slice(2)
+slice12 = slice(2)
+slice13 = slice(3)
+slice14 = slice(3)
+slice15 = slice(3)
+slice16 = slice(3)
+slice17 = slice(3)
+
+# print all the numbers
+print(string1[slice6])
+print(string3[slice7])
+print(string6[slice8])
+print(string10[slice9])
+print(string14[slice10])
+print(string15[slice10])
+print(string16[slice10])
+
 
 
