ae835b11c08505ac93d0d1e4bb354923c388edb4
damosman <48088578+damosman@users.noreply.github.com>
2022-01-27 16:49:58 +0100

    Merge remote-tracking branch 'origin/master'

--- Calculator.py
+++ Calculator.py
@@ -27,11 +27,20 @@ num1 = int(input("What's the first number?: "))
 
 for symbol in operators:
   print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
+first_operator_symbol = input("Pick an operation from the line above: ")
 
 num2 = int(input("What's the second number?: "))
 
-calculation_function = operators[operation_symbol]
-answer = calculation_function(num1, num2)
+calculation_function = operators[first_operator_symbol]
+first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {first_operator_symbol} {num2} = {first_answer}")
+
+second_operator_symbol = input("Pick another operator: ")
+num3 = int(input("What's the third number?: "))
+
+calculation_function = operators[second_operator_symbol]
+
+second_answer = calculation_function(first_answer, num3)
+
+print(f"{first_answer} {second_operator_symbol} {num3} = {second_answer}")
